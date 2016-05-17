4e3d382949546aaae444cf804465cdd764974b77
ruthimaria01 <mariaruthillena.mrf@gmail.com>
2016-05-17 08:41:44 -0300

    AULA2
    BIA

---
+++ bonequinhos aula2.cpp
@@ -0,0 +1,4 @@
+﻿1>------ Build started: Project: aula2, Configuration: Debug Win32 ------
+1>  bonequinho.cpp
+1>  aula2.vcxproj -> c:\users\20151170150070\documents\visual studio 2013\Projects\aula2\Debug\aula2.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
